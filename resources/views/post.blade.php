b94fb476461d5cea7e880a109bd63b3fea6b378f
Muhamad Rifqi Maulana S <mrifqims13@gmail.com>
2021-10-21 00:09:51 +0700

    Post category eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <h5>{{ $post->author }}</h5>
+        <p>By Muh Rifqi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
 
