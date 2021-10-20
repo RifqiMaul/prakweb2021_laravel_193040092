935c884c9a840d6ef210e5f7fe53e3cef6a1bc55
Muhamad Rifqi Maulana S <mrifqims13@gmail.com>
2021-10-20 22:55:47 +0700

    Membuat Model Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,13 @@
 
 namespace App\Models;
 
-class Post 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Maul",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptas 
-            voluptatibus libero eaque quasi, quaerat vel voluptatum impedit a? Cumque, non fuga 
-            corporis placeat pariatur repellendus ipsa, est asperiores nam voluptatum doloribus 
-            minus voluptates, sed veritatis earum labore hic explicabo velit? Doloribus dolorem 
-            aliquam numquam labore?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Rifqi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptas 
-            voluptatibus libero eaque quasi, quaerat vel voluptatum impedit a? Cumque, non fuga 
-            corporis placeat pariatur repellendus ipsa, est asperiores nam voluptatum doloribus 
-            minus voluptates, sed veritatis earum labore ic explicabo velit? Doloribus dolorem 
-            aliquam numquam labore."
-        ]
-    
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
